820f2bfc46ab00d5ce948fc7429dc118f0e290e9
jeonghoon <shb03323@naver.com>
2022-11-09 22:39:40 +0900

    init: 프로젝트 초기 세팅

---
+++ workit/src/main/java/workit/WorkitApplication.java
@@ -0,0 +1,13 @@
+package workit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkitApplication.class, args);
+    }
+
+}
---
+++ workit/src/test/java/workit/WorkitApplicationTests.java
@@ -0,0 +1,13 @@
+package workit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
