c62949eb0484d44d072d7aee37301cb7ffcc714c
jeonghoon <shb03323@naver.com>
2022-11-17 21:59:33 +0900

    feat: no value 예외처리

--- workit/src/main/java/workit/dto/work/WorkCreateRequestDto.java
+++ workit/src/main/java/workit/dto/work/WorkCreateRequestDto.java
@@ -1,5 +1,6 @@
 package workit.dto.work;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,9 +10,14 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class WorkCreateRequestDto {
+    @NotNull
     private Date date;
+    @NotNull
     private String projectTitle;
+    @NotNull
     private String workTitle;
+    @NotNull
     private String description;
+    @NotNull
     private List<Long> abilities;
 }
--- workit/src/main/java/workit/util/ExceptionHandler.java
+++ workit/src/main/java/workit/util/ExceptionHandler.java
@@ -1,13 +1,20 @@
 package workit.util;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = CustomException.class)
-    protected ResponseEntity<ResponseMessage> handleCustomException(CustomException e) {
-        return ResponseMessage.toResponseEntity(e.getResponseCode());
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = Exception.class)
+    protected ResponseEntity<ResponseMessage> handleCustomException(Exception e) {
+        if (e instanceof CustomException) {
+            return ResponseMessage.toResponseEntity(((CustomException) e).getResponseCode());
+        } else if (e instanceof MethodArgumentNotValidException || e instanceof HttpMessageNotReadableException) {
+            return ResponseMessage.toResponseEntity(new CustomException(ResponseCode.NO_VALUE_REQUIRED).getResponseCode());
+        }
+        System.out.println(e);
+        return ResponseMessage.toResponseEntity(new CustomException(ResponseCode.BAD_REQUEST).getResponseCode());
     }
 }
