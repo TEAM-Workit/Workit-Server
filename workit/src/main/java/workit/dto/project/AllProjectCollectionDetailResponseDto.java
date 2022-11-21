9842c550ccb65f72186a9e93e1dd948c0e74d622
dingding-21 <phj8221@naver.com>
2022-11-22 04:10:07 +0900

    refactor: 모아보기 공통 Dto로 이름 변경

--- workit/src/main/java/workit/dto/project/AllProjectCollectionDetailResponseDto.java
+++ workit/src/main/java/workit/dto/project/AllProjectCollectionDetailResponseDto.java
@@ -1,15 +1,16 @@
 package workit.dto.project;
 
 import lombok.Data;
+import workit.dto.collection.CollectionDetailResponseDto;
 
 import java.util.List;
 
 @Data
 public class AllProjectCollectionDetailResponseDto {
     private String projectTitle;
-    private List<ProjectCollectionDetailResponseDto> works;
+    private List<CollectionDetailResponseDto> works;
 
-    public AllProjectCollectionDetailResponseDto(String projectTitle, List<ProjectCollectionDetailResponseDto> works) {
+    public AllProjectCollectionDetailResponseDto(String projectTitle, List<CollectionDetailResponseDto> works) {
         this.projectTitle = projectTitle;
         this.works = works;
     }
