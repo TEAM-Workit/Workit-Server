ea33b0ceece4c71beafdd0dbbd36e799693f0e40
dingding-21 <phj8221@naver.com>
2023-06-10 00:29:48 +0900

    feat: 로그인 api responseBody에 username, email 추가

--- workit/src/main/java/workit/dto/user/LoginResponseDto.java
+++ workit/src/main/java/workit/dto/user/LoginResponseDto.java
@@ -6,6 +6,8 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class LoginResponseDto {
-    private String accessToken;
     private Long id;
+    private String username;
+    private String email;
+    private String accessToken;
 }
