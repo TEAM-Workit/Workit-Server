a5de1df83be40ba9916c2e42331dbdd73ae50e43
jeonghoon <shb03323@naver.com>
2023-06-11 23:18:26 +0900

    test: 테스트 코드 초기 설정

---
+++ workit/src/test/java/workit/integration/IntegrationTest.java
@@ -0,0 +1,18 @@
+package workit.integration;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+}
