41e714f7b44845cf50f02aa649095703280f535e
jeonghoon <shb03323@naver.com>
2023-07-29 11:22:56 +0900

    fix: 테스트 주석화

--- workit/src/test/java/workit/integration/WorkIntegrationTest.java
+++ workit/src/test/java/workit/integration/WorkIntegrationTest.java
@@ -1,32 +1,32 @@
-package workit.integration;
-
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
-
-import static io.restassured.RestAssured.given;
-
-public class WorkIntegrationTest extends IntegrationTest {
-
-    @Nested
-    @DisplayName("전체 워킷 조회 API - POST /work")
-    class GetAll {
-
-        @Test
-        @DisplayName("성공")
-        void success() {
-//            // given
-//            Long userId = 1L;
+//package workit.integration;
 //
-//            // when
-//            given()
-//                    .auth().o.basic(member.getEmail(), "invalid password")
-//                    .when().post("/orders")
-//                    .then()
-//                    .extract();
-
-        }
-    }
-}
+//import io.restassured.RestAssured;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Nested;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.http.MediaType;
+//
+//import static io.restassured.RestAssured.given;
+//
+//public class WorkIntegrationTest extends IntegrationTest {
+//
+//    @Nested
+//    @DisplayName("전체 워킷 조회 API - POST /work")
+//    class GetAll {
+//
+//        @Test
+//        @DisplayName("성공")
+//        void success() {
+////            // given
+////            Long userId = 1L;
+////
+////            // when
+////            given()
+////                    .auth().o.basic(member.getEmail(), "invalid password")
+////                    .when().post("/orders")
+////                    .then()
+////                    .extract();
+//
+//        }
+//    }
+//}
